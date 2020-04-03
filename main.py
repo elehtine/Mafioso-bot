96533fe0da0ce8b76cfba1f79e19056734570557
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-04-03 13:57:46 +0300

    Initial commit, simple game bot

---
+++ main.py
@@ -0,0 +1,97 @@
+import logging
+
+import random
+import sys
+
+import telegram
+from telegram.ext import  Updater, CommandHandler, MessageHandler
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+ROLES = [ 'ROLE 0', 'ROLE 1', 'ROLE 2', 'ROLE 3', 'ROLE 4', 'ROLE 5', 'ROLE 6', 'ROLE 7', 'ROLE 8', 'ROLE 9' ]
+
+
+def start(update, context):
+    """Tell all commands to user"""
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm bot!!!")
+
+def new_game(update, context):
+    """Create new game of mafioso"""
+
+    # Delete old dict and create new one
+    if 'mafioso' in context.bot_data:
+        del context.bot_data['mafioso']
+    context.bot_data['mafioso'] = list()
+
+    # Send message for success
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Game created!!")
+
+def join_game(update, context):
+    """Add message sender to game"""
+
+    # check if game is created
+    if 'mafioso' not in context.bot_data:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Game is not created yet")
+        return
+
+    user = update.effective_user.id
+    data = context.bot_data['mafioso']
+
+    # check if already joined
+    if user in data:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You are already joined")
+        return
+    data.append(user)
+
+    # Send message for success
+    context.bot.send_message(chat_id=update.effective_chat.id, text="You are added to game!!")
+
+def begin_game(update, context):
+    """Begin game and give roles to players"""
+
+    # Check if game is created
+    if 'mafioso' not in context.bot_data:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Game is not created yet")
+        return
+
+    # Get player roles, shuffle them and send to all players
+    players = context.bot_data['mafioso']
+    num = len(players)
+    roles = [ ROLES[i] for i in range(num) ]
+    random.shuffle(roles)
+
+    for player, role in zip(players, roles):
+        context.bot.send_message(chat_id=player, text=f"Player {player} has role {role}")
+
+    # Send message for success
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Roles were send")
+
+
+def help_handler(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="List of commands\n"
+            "/start, /new, /join, /begin, /help")
+
+def main():
+    if len(sys.argv) > 1:
+        token = sys.argv[1]
+    else:
+        token = "TOKEN"
+
+    print("Your token is", token)
+    updater = Updater(token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('new', new_game))
+    dispatcher.add_handler(CommandHandler('join', join_game))
+    dispatcher.add_handler(CommandHandler('begin', begin_game))
+    dispatcher.add_handler(CommandHandler('help', help_handler))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__=='__main__':
+    main()
